281758e107527329d0af5158ac9a862a4839f9de
Deepti Thoughtwin <deepti@thoughtwin.com>
2026-06-30 09:02:43 +0530

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,9 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     caption = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+    def __str__(self):
+        return f"{self.user.username} - {self.caption[:30] if self.caption else 'No Caption'}"
 
 
 class PostMedia(models.Model):
@@ -17,6 +19,9 @@ class PostMedia(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="media")
     file = models.FileField(upload_to="posts/media/")
     media_type = models.CharField(max_length=10, choices=MEDIA_TYPE)
+
+    def __str__(self):
+        return f"{self.post.user.username} - {self.media_type.capitalize()}"
 
 
 
