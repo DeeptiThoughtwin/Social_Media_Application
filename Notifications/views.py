5049a44ea6f230bc12a644c76bcc983a0619f803
Deepti Thoughtwin <deepti@thoughtwin.com>
2026-07-09 03:12:25 +0530

    Update views.py

--- Notifications/views.py
+++ Notifications/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Notification
+from django.http import JsonResponse
 
 
 @login_required
 def notifications(request):
     notifications = Notification.objects.filter(receiver=request.user).order_by("-created_at")
     return render(request,"notifications.html",{"notifications": notifications})
+
+
+
+@login_required
+def notification_count(request):
+
+    count = request.user.received_notifications.filter(
+        is_read=False
+    ).count()
+
+    return JsonResponse({
+        "count": count
+    })
